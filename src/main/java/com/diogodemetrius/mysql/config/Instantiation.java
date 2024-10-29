bb64f98934741173738a0792cfd350af92802c46
Diogo Demetrius <diogod.mmoreira@gmail.com>
2024-10-29 04:58:16 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/diogodemetrius/mysql/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.diogodemetrius.mysql.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.diogodemetrius.mysql.domain.User;
+import com.diogodemetrius.mysql.repository.UserRepository;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        userRepository.deleteAll();
+        
+        // Use null para o id, permitindo que o banco de dados gere automaticamente
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        
+        // Usar saveAll para salvar todos de uma vez
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
--- src/main/java/com/diogodemetrius/mysql/domain/User.java
+++ src/main/java/com/diogodemetrius/mysql/domain/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;  
 
     private String name;
     private String email;
@@ -23,18 +23,18 @@ public class User implements Serializable {
     public User() {
     }
 
-    public User(String id, String name, String email) {
-        this.id = id;
+    public User(Long id, String name, String email) {  
+        this.id = id;  
         this.name = name;
         this.email = email;
     }
 
     // Getters e Setters
-    public String getId() {
+    public Long getId() {  
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
