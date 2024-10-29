c560cb6fdb40c4411258cb17af1574140178147d
Diogo Demetrius <diogod.mmoreira@gmail.com>
2024-10-29 02:58:07 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/com/diogodemetrius/mysql/MysqlprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogodemetrius.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
